b478d863798aa15063358fe64433f08bec883da5
joseherrera7 <31715468+joseherrera7@users.noreply.github.com>
2018-03-13 16:24:10 -0600

    Creando proyecto de Lab 3
    Subiendo archivos

---
+++ Lab3/Lab3/Models/Partido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab3.Models
+{
+    public class Partido
+    {
+        int NoPartido { get; set; }
+        DateTime FechaPartido { get; set; }
+        string Grupo { get; set; }
+        string Pais1 { get; set; }
+        string Pais2 { get; set; }
+        string Estadio { get; set; }
+    }
+}
