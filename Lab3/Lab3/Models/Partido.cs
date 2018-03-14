893eff837c68d32160287b4f833813ca35e675e8
joseherrera7 <31715468+joseherrera7@users.noreply.github.com>
2018-03-13 19:58:32 -0600

    Subiendo archivos y arbol aun no implementado

--- Lab3/Lab3/Models/Partido.cs
+++ Lab3/Lab3/Models/Partido.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace Lab3.Models
 {
     public class Partido
     {
+        [Key]
         int NoPartido { get; set; }
+        [Key]
         DateTime FechaPartido { get; set; }
         string Grupo { get; set; }
         string Pais1 { get; set; }
